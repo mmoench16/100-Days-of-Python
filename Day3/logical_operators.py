df43a72b88eb371ba62c97ae56726c6c70dff2c9
mmoench16 <m.moench16@gmail.com>
2024-05-16 13:20:39 +0100

    Day3 complete.
    Created "treasure island" game.

--- Day3/logical_operators.py
+++ Day3/logical_operators.py
@@ -13,6 +13,9 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        bill = 0
+        print("You ride for free.")
     else:
         bill = 12
         print("Adult tickets are $12.")
