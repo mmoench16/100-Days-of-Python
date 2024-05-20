484e03f1ba2fda563a9986642ceaa6e3d28fd10d
mmoench16 <m.moench16@gmail.com>
2024-05-20 17:15:20 +0100

    Create lists.py

---
+++ Day4/lists.py
@@ -0,0 +1,18 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia"]
+
+print(states_of_america[-1])
+
+states_of_america.append("Angelaland")
+
+print(states_of_america)
+
+states_of_america.extend(["Virginia", "New York"])
+
+print(states_of_america)
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
