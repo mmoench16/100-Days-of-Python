5eb2231c08c2d6aba6c0d4f9b4a9ea619091dc4b
mmoench16 <m.moench16@gmail.com>
2024-05-22 17:31:21 +0100

    Day5 complete

---
+++ Day5/loops.py
@@ -0,0 +1,16 @@
+# fruits = ["Apple", "Peach", "Pear"]
+
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " Pie")
+    
+# print(fruits)
+
+# for number in range(1, 11, 3):
+#     print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+
+print(total)
