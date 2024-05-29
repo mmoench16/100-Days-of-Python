76fcac711b476ceb91548deba7892f20cb025478
mmoench16 <m.moench16@gmail.com>
2024-05-29 17:03:31 +0100

    Create function_mult_args.py

---
+++ Day8/function_mult_args.py
@@ -0,0 +1,6 @@
+# Functions with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with(name = "Martin", location = "Troon")
