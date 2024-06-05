cd19c564debf8b4a3fe87fb41af6f11b6650f0c6
mmoench16 <m.moench16@gmail.com>
2024-06-05 19:30:21 +0100

    Update main.py

--- Day10/main.py
+++ Day10/main.py
@@ -1,6 +1,10 @@
 # Functions with Outputs
 
 def format_name(f_name, l_name):
+
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+
     full_name = f_name.title() + " " + l_name.title()
     return full_name
 
