6686d7ebe455b4eb0d3e2ffefc0cf8219cce5d6f
mmoench16 <m.moench16@gmail.com>
2024-06-21 16:40:46 +0100

    Started on Quiz.

---
+++ Day17/quiz-game-start/main.py
@@ -0,0 +1,9 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    question_bank.append(Question(question["text"], question["answer"]))
+
+print(question_bank)
