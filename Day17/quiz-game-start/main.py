92636b05430014ceaa1f5e829c48f48efbbca6c3
mmoench16 <m.moench16@gmail.com>
2024-06-24 09:04:29 +0100

    Day 17 complete. Day 18 started.

--- Day17/quiz-game-start/main.py
+++ Day17/quiz-game-start/main.py
@@ -5,9 +5,12 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    question_bank.append(Question(question["text"], question["answer"]))
+    question_bank.append(Question(question["question"], question["correct_answer"]))
 
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You have completed the quiz.")
+print(f"Your final score was: {quiz.score} / {quiz.question_number}")
