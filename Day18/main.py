827c4057089113afd58681aa88a0afa927e14b5a
mmoench16 <m.moench16@gmail.com>
2024-06-25 16:18:44 +0100

    Progress on Day18.

--- Day18/main.py
+++ Day18/main.py
@@ -1,13 +1,32 @@
 from turtle import Turtle, Screen
-
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-
-
-
+import random
+# from turtle import *
+# import turtle as t
 
 screen = Screen()
+screen.colormode(255)
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
+
+
+# Dashed line
+# for _ in range(10):
+#     tim.forward(5)
+#     tim.up()
+#     tim.forward(5)
+#     tim.down()
+
+# Multiple shapes
+for i in range(3,11):
+    for _ in range(i):
+        tim.forward(100)
+        tim.right(360/i)
+
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    tim.color(r,g,b)
+
 screen.exitonclick()
