df8810a6b34b95754f98025c85bfcfdd7a2b01df
mmoench16 <m.moench16@gmail.com>
2024-06-26 16:49:56 +0100

    Day18 continued.

--- Day18/main.py
+++ Day18/main.py
@@ -8,8 +8,17 @@ screen.colormode(255)
 
 tim = Turtle()
 tim.shape("turtle")
-tim.color("red")
+tim.speed("fastest")
 
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+
+    return (r,g,b)
+
+# tim.pensize(5)
+# tim.forward(25)
 
 # Dashed line
 # for _ in range(10):
@@ -19,14 +28,31 @@ tim.color("red")
 #     tim.down()
 
 # Multiple shapes
-for i in range(3,11):
-    for _ in range(i):
-        tim.forward(100)
-        tim.right(360/i)
+# for i in range(3,11):
+#     for _ in range(i):
+#         tim.forward(100)
+#         tim.right(360/i)
 
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b = random.randint(0,255)
-    tim.color(r,g,b)
+#     r = random.randint(0,255)
+#     g = random.randint(0,255)
+#     b = random.randint(0,255)
+#     tim.color(r,g,b)
+
+# Random Walk
+# angles = [0,90,180,270]
+# for _ in range(200):
+#     tim.color(random_color())
+#     tim.forward(20)
+#     tim.setheading(random.choice(angles))    
+
+# Spirograph
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.setheading(tim.heading() + size_of_gap)
+        tim.color(random_color())
+        tim.circle(radius=100)
+
+draw_spirograph(5)
 
 screen.exitonclick()
