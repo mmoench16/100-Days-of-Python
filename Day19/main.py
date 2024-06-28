19f2254b245acff4d83ea894d272274b9ab704da
mmoench16 <m.moench16@gmail.com>
2024-06-28 16:47:13 +0100

    Day19 complete.

--- Day19/main.py
+++ Day19/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
 tim = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)
@@ -15,5 +17,21 @@ for colour in colours:
     t.goto(x=-230,y=-75 + i)
     turtles.append(t)
     i += 30
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winner = turtle.pencolor()
+            if winner == user_bet:
+                print(f"You've won! The {winner} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winner} turtle is the winner!")
+
+        rand_distance = random.randint(0,10)
+        turtle.forward(rand_distance)
 
 screen.exitonclick()
