1b3e299fe18c0cc6fd71f8305f9c8b36ddbbeed2
mmoench16 <m.moench16@gmail.com>
2024-07-16 16:41:07 +0100

    Day25 complete. Day26 started..

--- Day25/main.py
+++ Day25/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("US States Game")
@@ -7,12 +8,33 @@ screen.addshape(img)
 
 turtle.shape(img)
 
-answer_state = screen.textinput(title="Guess the State",
-                                prompt="What's another state?")
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
 
-print(answer_state)
+states = pandas.read_csv("50_states.csv")
 
+text_input_title = "Guess the State"
+correct_guesses = []
 
+while len(correct_guesses) < 50:
+    answer_state = screen.textinput(title=text_input_title,
+                                prompt="What's another state?").title()
+    
+    if answer_state == "Exit":
+        break
+    
+    if answer_state in states.state.values:
+        s = states[states["state"] == answer_state]
+        t.goto(s.x.item(), s.y.item())
+        t.write(answer_state)
+        correct_guesses.append(answer_state)
+        text_input_title = f"{len(correct_guesses)}/50 States Correct"
+
+not_guessed_states = states[~states.state.isin(correct_guesses)]
+not_guessed_states.state.to_csv("states_to_learn.csv")
+
+# How the coordinates for the State names were created.
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
 
