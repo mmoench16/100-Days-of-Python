aa719dbe89c2db1bbe0d8c5297922f353b4a3748
mmoench16 <m.moench16@gmail.com>
2024-07-25 13:41:25 +0100

    Day28 complete. Day 29 started.

--- Day28/main.py
+++ Day28/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,12 +9,49 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    global reps
+    window.after_cancel(timer)
+    header_label.config(text="Timer")
+    canvas.itemconfig(timer_text, text="00:00")
+    check_label.config(text="")
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        header_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        header_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        header_label.config(text="Work", fg=GREEN)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    global timer
+    count_minutes = math.floor(count / 60)
+    count_seconds = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_minutes}:{count_seconds:02d}")
+    if count > 0:
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        # if (reps-1) % 2 == 0:
+        sets_completed = math.floor((reps-1) / 2)
+        check_label.config(text="".join(map(lambda x: x*sets_completed, "✔")), fg=GREEN, bg=YELLOW)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,7 +61,7 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="Day28/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1,row=1)
 
 # Header Label
@@ -31,14 +69,14 @@ header_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 header_label.grid(column=1,row=0)
 
 # Buttons
-start_button = Button(text="start", highlightbackground=YELLOW, highlightthickness=0)
+start_button = Button(text="start", highlightbackground=YELLOW, highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightbackground=YELLOW, highlightthickness=0)
+reset_button = Button(text="Reset", highlightbackground=YELLOW, highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 # Check Mark Label
-check_label = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_label = Label(fg=GREEN, bg=YELLOW)
 check_label.grid(column=1,row=3)
 
 window.mainloop()
