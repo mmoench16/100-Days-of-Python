20057af0c052b47aad1f0070cf6086d27b53ee90
mmoench16 <m.moench16@gmail.com>
2024-07-29 16:59:05 +0100

    Day29 dine, day 30 started.

--- Day29/main.py
+++ Day29/main.py
@@ -1,7 +1,42 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = []
+
+    password_list.extend(choice(letters) for _ in range(randint(8, 10)))
+    password_list.extend(choice(symbols) for _ in range(randint(2, 4)))
+    password_list.extend(choice(numbers) for _ in range(randint(2, 4)))
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\nUsername/Email: {username}\nPassword: {password}\nIs it OK to save?")
+
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(website + " | " + username + " | " + password + "\n")
+
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -10,7 +45,7 @@ window.config(padx=50, pady=50)
 
 # Canvas & Logo
 canvas = Canvas(width=200, height=200, highlightthickness=0)
-logo = PhotoImage(file="Day29/logo.png")
+logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo)
 canvas.grid(column=1, row=0)
 
@@ -27,18 +62,20 @@ password_label.grid(column=0, row=3)
 # Entries
 website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 username_entry = Entry(width=35)
 username_entry.grid(column=1, row=2, columnspan=2)
+username_entry.insert(0, "email@email.com")
 
 password_entry = Entry(width=18)
 password_entry.grid(column=1, row=3)
 
 # Buttons
-gen_pass_btn = Button(text="Generate Password")
+gen_pass_btn = Button(text="Generate Password", command=generate_password)
 gen_pass_btn.grid(column=2, row=3)
 
-add_btn = Button(text="Add", width=33)
+add_btn = Button(text="Add", width=33, command=save_password)
 add_btn.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
