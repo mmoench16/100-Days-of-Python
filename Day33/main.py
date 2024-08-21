abd0ae6648b994b6a15dab6ee5f92a912c2cdc69
mmoench16 <m.moench16@gmail.com>
2024-08-21 13:36:38 +0100

    Day34 started.

--- Day33/main.py
+++ Day33/main.py
@@ -1,27 +1,80 @@
-from tkinter import *
 import requests
+from datetime import datetime
+import time
+import smtplib
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-def get_quote():
-    response = requests.get(url="https://api.kanye.rest")
+MY_LAT = 55.543781
+MY_LONG = -4.664010
+
+iss_latitude = 0.0
+iss_longitude = 0.0
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+def get_iss_position():
+    global iss_latitude
+    global iss_longitude
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     data = response.json()
-    canvas.itemconfig(quote_text, text=f"{data["quote"]}")
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
-canvas.grid(row=0, column=0)
+#Your position is within +5 or -5 degrees of the ISS position.
+def within_5_degrees():
+    within = False
+    
+    if ((MY_LAT - 5 <= iss_latitude <= MY_LAT + 5)
+        and (MY_LONG - 5 <= iss_longitude <= MY_LONG + 5)):
+        within = True
+    
+    return within
 
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
+def is_dark():
+    is_dark = False
 
-get_quote()
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-window.mainloop()
+    time_now = datetime.now()
+
+    if time_now.hour <= sunrise or time_now.hour >= sunset:
+        is_dark = True
+
+    return is_dark
+
+def send_email():
+    my_email = os.getenv("MY_EMAIL")
+    password = os.getenv("APP_PASSWORD")
+    recipient_email = os.getenv("RECIPIENT_EMAIL")
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, 
+                            to_addrs=recipient_email, 
+                            msg=f"Subject:ISS Alert\n\nYou should be able to see the ISS now (weather permitting).")
+
+while True:
+    get_iss_position()
+    if within_5_degrees():
+        if is_dark():
+            send_email()
+    time.sleep(60)
+
+#If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
--- Day33/main1.py
+++ Day33/main1.py
@@ -1,12 +1,35 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 55.543781
+MY_LNG = -4.664010
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 
 data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+print(sunrise.split("T")[1].split(":"))
 
-iss_position = (longitude, latitude)
-print(iss_position)
+time_now = datetime.now()
+
+print(time_now.hour)
