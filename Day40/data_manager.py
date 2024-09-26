92b19f1a0ded5ad23b7ea5d430ec0a2710438071
mmoench16 <m.moench16@gmail.com>
2024-09-26 17:16:15 +0100

    Day 40 started.

---
+++ Day40/data_manager.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/315a5b9eabed04aec7a5e0d7b417ab17/flightDeals/prices"
+
+
+class DataManager:
+
+    def __init__(self):
+        self.sheety_auth_token = { "Authorization": f"Bearer {os.getenv("SHEETY_AUTH_TOKEN")}" }
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        # Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT, headers= self.sheety_auth_token)
+        data = response.json()
+        self.destination_data = data["prices"]
+        # Try importing pretty print and printing the data out again using pprint() to see it formatted.
+        # pprint(data)
+        return self.destination_data
+
+    # In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data,
+                headers= self.sheety_auth_token
+            )
+            print(response.text)
