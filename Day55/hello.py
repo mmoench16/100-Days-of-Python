5c943ffe679d91e1184231b763fd1e10caa0bd72
mmoench16 <m.moench16@gmail.com>
2024-12-19 12:01:47 +0000

    Day55 continued

--- Day55/hello.py
+++ Day55/hello.py
@@ -2,6 +2,39 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Decorator functions ⬇️
+def make_bold(func):
+    """
+    Decorator function that wraps the output of the given function
+    in HTML bold tags (<b></b>), which typically bolds the text in HTML.
+    """
+    def wrapper():
+        return f"<b>{func()}</b>"
+    return wrapper
+
+def make_emphasis(func):
+    """
+    Decorator function that wraps the output of the given function
+    in HTML emphasis tags (<em></em>), which typically italicizes
+    the text in HTML.
+    """
+
+    def wrapper():
+        return f"<em>{func()}</em>"
+    return wrapper
+# Decorator functions ⬆️
+
+def make_underlined(func):
+    """
+    Decorator function that wraps the output of the given function
+    in HTML underline tags (<u></u>), which typically underlines
+    the text in HTML.
+    """
+
+    def wrapper():
+        return f"<u>{func()}</u>"
+    return wrapper
+
 @app.route("/")
 def hello_world():
     return "<h1 style='text-align:center'>Hello, World!" \
@@ -9,6 +42,9 @@ def hello_world():
         "<img src='https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGpxY3RieTI4emd2ODQzc2IyZ3hhZTdrcTYzdWNlaHowZTYyOGkxNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oriO0OEd9QIDdllqo/giphy.gif'>"
 
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
 def say_bye():
     return "<p>Bye</p>"
 
@@ -18,3 +54,4 @@ def greet(name):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
