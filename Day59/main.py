9f0bdf0e5c8d7ed6684a99d1e4f401586c1f72d3
mmoench16 <m.moench16@gmail.com>
2025-03-08 09:14:01 +0000

    Day59 finished, 60 started

--- Day59/main.py
+++ Day59/main.py
@@ -3,9 +3,13 @@ import requests
 
 app = Flask(__name__)
 
+response = requests.get("https://api.npoint.io/8be439e2d02333d46d42")
+response.raise_for_status()
+all_posts = response.json()
+
 @app.route("/")
 def get_all_posts():
-    return render_template("index.html")
+    return render_template("index.html", posts=all_posts)
 
 @app.route("/about")
 def about():
@@ -14,6 +18,11 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/post/<int:post_id>")
+def get_post(post_id):
+    post = next((post for post in all_posts if post["id"] == post_id), None)
+    return render_template("post.html", post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
