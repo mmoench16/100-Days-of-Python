d911fd211d5a64c9fb2426622f5caff838c05d5a
mmoench16 <m.moench16@gmail.com>
2025-03-25 21:30:49 +0000

    Day 63 continued.

--- Day63/main.py
+++ Day63/main.py
@@ -1,28 +1,55 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
 
-all_books = []
+# Configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+# Initialize the app with the extension
+db.init_app(app)
 
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars()
     return render_template("index.html", books=all_books)
-
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
+        try:
+            rating = float(request.form["rating"])
+            new_book = Book(
+                title=request.form["title"],
+                author=request.form["author"],
+                rating=rating
+            )
+            db.session.add(new_book)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return f"An error occurred: {e}", 400
         return redirect(url_for("home"))
-    
     return render_template("add.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Day63_SQLite/main.py
+++ Day63_SQLite/main.py
@@ -36,3 +36,30 @@ with app.app_context():
     )
     db.session.add(new_book)
     db.session.commit()
+
+with app.app_context():
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars()
+
+with app.app_context():
+    book = db.session.execute(db.select(Book).where(Book.title == "Harry Potter")).scalar()
+
+with app.app_context():
+  book_to_update = db.session.execute(db.select(Book).where(Book.title == "Harry Potter")).scalar()
+  book_to_update.title = "Harry Potter and the Chamber of Secrets"
+  db.session.commit()
+
+book_id = 1
+with app.app_context():
+  # or book_to_update = db.get_or_404(Book, book_id) 
+  book_to_update = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+  book_to_update.title = "Harry Potter and the Goblet of Fire"
+  db.session.commit()
+
+book_id = 1
+with app.app_context():
+  book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+  # or book_to_delete = db.get_or_404(Book, book_id)
+  db.session.delete(book_to_delete)
+  db.session.commit()
+    
