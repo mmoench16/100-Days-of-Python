495c9c9021f017f63c071f9fb64c87510f5ed600
mmoench16 <m.moench16@gmail.com>
2025-06-15 18:55:27 +0100

    Day66
    Well actually it's day 66 😉

---
+++ Day65/main.py
@@ -0,0 +1,116 @@
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean
+import random
+
+app = Flask(__name__)
+
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+# Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# Cafe TABLE Configuration
+class Cafe(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    map_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    location: Mapped[str] = mapped_column(String(250), nullable=False)
+    seats: Mapped[str] = mapped_column(String(250), nullable=False)
+    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# HTTP GET - Read Record
+@app.route("/random")
+def get_random_cafe():
+    cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(cafes)
+    return jsonify({
+        "cafe": {
+            "id": random_cafe.id,
+            "name": random_cafe.name,
+            "map_url": random_cafe.map_url,
+            "img_url": random_cafe.img_url,
+            "location": random_cafe.location,
+            "seats": random_cafe.seats,
+            "has_toilet": random_cafe.has_toilet,
+            "has_wifi": random_cafe.has_wifi,
+            "has_sockets": random_cafe.has_sockets,
+            "can_take_calls": random_cafe.can_take_calls,
+            "coffee_price": random_cafe.coffee_price
+            }
+        })
+
+@app.route("/all")
+def get_all_cafes():
+    cafes = db.session.query(Cafe).all()
+    all_cafes = []
+    for cafe in cafes:
+        all_cafes.append({
+            "id": cafe.id,
+            "name": cafe.name,
+            "map_url": cafe.map_url,
+            "img_url": cafe.img_url,
+            "location": cafe.location,
+            "seats": cafe.seats,
+            "has_toilet": cafe.has_toilet,
+            "has_wifi": cafe.has_wifi,
+            "has_sockets": cafe.has_sockets,
+            "can_take_calls": cafe.can_take_calls,
+            "coffee_price": cafe.coffee_price
+        })
+    return jsonify({"cafes": all_cafes})
+
+@app.route("/search")
+def search_cafe():
+    location = request.args.get("loc")
+    cafes = db.session.query(Cafe).where(Cafe.location == location).all()
+    all_cafes = []
+    
+    if cafes:
+        for cafe in cafes:
+            all_cafes.append({
+                "id": cafe.id,
+                "name": cafe.name,
+                "map_url": cafe.map_url,
+                "img_url": cafe.img_url,
+                "location": cafe.location,
+                "seats": cafe.seats,
+                "has_toilet": cafe.has_toilet,
+                "has_wifi": cafe.has_wifi,
+                "has_sockets": cafe.has_sockets,
+                "can_take_calls": cafe.can_take_calls,
+                "coffee_price": cafe.coffee_price
+            })
+        return jsonify({"cafes": all_cafes}), 200
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
+
+# HTTP POST - Create Record
+
+# HTTP PUT/PATCH - Update Record
+
+# HTTP DELETE - Delete Record
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
